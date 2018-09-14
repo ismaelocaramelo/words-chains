06ea4d051678dd221674ed39bb01e607bd5a29a3
Ismael Bakkali <ismaelbacha@hotmail.com>
2018-09-14 16:37:51 +0200

    Push process

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Word Chains
+ * Kata 14-09-2018.
+ * GuideSmiths Spain
+ * ======================
+ * @author Ismael, German
+ */
+
+const axios = require('axios');
+
+const API_URL = 'https://wordsapiv1.p.mashape.com/words/';
+
+function find($word) {}
+
+// Preguntar por la primera palabra
+// Preguntar por la segunda palabra
+
+// Check two words has the same length
+// If not make process again
+
+function init() {
+  process.stdout.write('Enter first letter ' + '\n');
+}
+
+init();
